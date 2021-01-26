975dfb8ab55ea05c3c73a5ea9530d9f5ef234cf4
colie31 <colie4272@gmail.com>
2021-01-26 16:57:14 -0500

    added mainpage and user component in app.js

---
+++ react-app/src/components/MainPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const MainPage = () => {
+    return (
+        <>
+        <h1>BrainBust Main Page</h1>
+        </>
+    )
+}
+
+export default MainPage;
