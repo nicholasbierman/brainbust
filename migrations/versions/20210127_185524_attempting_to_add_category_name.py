515d574a53c0fbfc24b6ac80c71d6a5808369048
colie31 <colie4272@gmail.com>
2021-01-27 19:00:19 -0500

    updating category column

---
+++ migrations/versions/20210127_185524_attempting_to_add_category_name.py
@@ -0,0 +1,24 @@
+"""attempting to add category name
+
+Revision ID: 012420e23484
+Revises: 930833c3caaf
+Create Date: 2021-01-27 18:55:24.690907
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '012420e23484'
+down_revision = '930833c3caaf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("categories", sa.Column("name", sa.String("le")))
+
+
+def downgrade():
+    pass
