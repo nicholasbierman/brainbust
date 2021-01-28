0b717beebe98e158193913896365aacc9a368507
Nick Bierman <biermannick83@gmail.com>
2021-01-28 17:48:49 -0500

    add GET /api/categories route

--- app/api/category_routes.py
+++ app/api/category_routes.py
@@ -4,11 +4,11 @@ from app.models import Category, db
 category_routes = Blueprint('categories', __name__)
 
 @category_routes.route('/', methods=["GET"])
-def categories():
+def get_categories():
     categories = Category.query.all()
     return {"categories": [category.to_dict() for category in categories]}
 
 @category_routes.route('/<int:id>')
-def category(id):
+def get_category(id):
     category = Category.query.get(id)
     return category.to_dict()
