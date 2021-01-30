10522fcf8d1df3d409b7ab774285f0da7f476c59
Nhu-y Phan <nhucleus@gmail.com>
2021-01-30 01:17:28 -0500

    played with some styling

--- react-app/src/components/DemoLoginFormModal/index.js
+++ react-app/src/components/DemoLoginFormModal/index.js
@@ -5,9 +5,17 @@ import DemoLoginForm from './DemoLoginForm';
 function DemoLoginFormModal() {
   const [showModal, setShowModal] = useState(false);
 
+  const style = {
+    background: 'linear-gradient(0deg, rgba(161,91,226,0.8793625356125356) 0%, rgba(40,27,255,1) 100%)',
+    border: 0,
+    color: 'white',
+    height: '100px',
+    padding: '0 30px',
+  };
+
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Demo Login</button>
+      <button style={style} onClick={() => setShowModal(true)}>Demo Login</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <DemoLoginForm />
