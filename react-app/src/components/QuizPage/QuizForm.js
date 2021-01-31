b5cc828997305ee3ac7c21dfb701e2617c62efd8
colie31 <colie4272@gmail.com>
2021-01-31 13:02:37 -0500

    added css to quizform

--- react-app/src/components/QuizPage/QuizForm.js
+++ react-app/src/components/QuizPage/QuizForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom"
 import { getQuestions } from "../../store/question";
+import "./QuizPage.css"
 
 const QuizForm = ({ amountOfQuestions }) => {
     const history = useHistory();
