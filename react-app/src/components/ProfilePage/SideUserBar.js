f38a8ddaf112b8db20bd39d200c5b7c8d3245d0c
Nhu-y Phan <nhucleus@gmail.com>
2021-01-30 22:15:23 -0500

    edited sidebar

--- react-app/src/components/ProfilePage/SideUserBar.js
+++ react-app/src/components/ProfilePage/SideUserBar.js
@@ -1,8 +1,52 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { addSingleQuiz } from '../../store/quiz';
+
 
 const SideUserBar = () => {
+    const user = useSelector(state => state.session.user);
+    const [name, setName] = useState('');
+    const [isPrivate, setIsPrivate] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState();
+    const dispatch = useDispatch();
+    const categories = useSelector(state => state.categories.categories);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(
+            addSingleQuiz({
+                name,
+                is_private: isPrivate,
+                category_id: parseInt(selectedCategory),
+            })
+        );
+    };
+    
+    
     return (
-        <h1>Side User Bar</h1>
+        <div>
+            <h1 className="sidebar">Welcome, {user.username}!</h1>
+            <>
+                <form onSubmit={handleSubmit}>
+                    <h1>Create A New Quiz</h1>
+                    <h2>A Quiz is a set of Questions</h2>
+                    <input name="name" type="text" placeholder="e.g. Biology 101, Constitutional Law" onChange={(e) => setName(e.target.value)} value={name}></input>
+                    <br></br>
+                    <label for="category">Category</label>
+                    <select onChange={(e) => setSelectedCategory(e.target.value)} name="category">
+                        {categories.map((category) => {
+                            return <option value={category.id}>{category.name}</option>
+                        })})
+                </select>
+                    <br></br>
+                    <label for="is_private">Private?</label>
+                    <input value={isPrivate} onChange={(e) => setIsPrivate(true)} name="is_private" type="checkbox"></input>
+                    <br></br>
+                    <button type="submit">Continue</button>
+                </form>
+            </>
+        </div>
     )
 }
 
--- react-app/src/components/ProfilePage/TopDisplay.js
+++ react-app/src/components/ProfilePage/TopDisplay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TopDisplay = () => {
-  return <h1>Top Display</h1>;
+  return <h1>Your Quizzes:</h1>;
 };
 
 export default TopDisplay;
