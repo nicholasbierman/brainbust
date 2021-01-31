c00fdcbdac265fac77b4d612eb6ea9d42df1f20d
colie31 <colie4272@gmail.com>
2021-01-30 21:05:16 -0500

    working quiz form

--- react-app/src/components/QuizPage/QuizForm.js
+++ react-app/src/components/QuizPage/QuizForm.js
@@ -10,6 +10,7 @@ const QuizForm = ({ amountOfQuestions }) => {
     const questions = useSelector((state) => state.questions.ques);
     const [index, setIndex] = useState(0)
     const [question, setQuestion] = useState({});
+    const [score, setScore] = useState(0)
 
     useEffect(() => {
       console.log(id)
@@ -24,23 +25,50 @@ const QuizForm = ({ amountOfQuestions }) => {
         setQuestion(questions[index])
     },[index])
 
-    let array;
-    if(question) {
-        array = [
-        "answer_1", 
-        "answer_2", 
-        "answer_3", 
-        "correct_answer"
-    ].map((k) => question[k]);
+    let answers = {};
+    if (question) {
+        answers = {
+          answer_1: question["answer_1"],
+          answer_2: question["answer_2"],
+          answer_3: question["answer_3"],
+          correct_answer: question["correct_answer"],
+        };
     }
 
-    const randomAnswer = () => {
-        const randomNumber = Math.floor(Math.random() * array.length)
-        return array.splice(randomNumber, 1)
+    let randomAnswerObj = { 1: null, 2: null, 3: null, 4: null };
+    const randomize = (answerObj, answer) => {
+      let randomChoice = Math.floor(Math.random() * 4) + 1;
+      if (!answerObj[randomChoice]) {
+        answerObj[randomChoice] = answer;
+      } else {
+        randomize(answerObj, answer);
+      }
+    };
+
+    for (let answer in answers) {
+      randomize(randomAnswerObj, { [answer]: answers[answer] });
     }
 
-    const handleClick = (e) => {
-        console.log(questions)
+    // let array;
+    // if(question) {
+    //     array = [
+    //     "answer_1", 
+    //     "answer_2", 
+    //     "answer_3", 
+    //     "correct_answer"
+    // ].map((k) => question[k]);
+    // }
+
+    // const randomAnswer = () => {
+    //     const randomNumber = Math.floor(Math.random() * array.length)
+    //     return array.splice(randomNumber, 1)
+    // }
+
+    const handleClick = (choice) => {
+        if (randomAnswerObj[choice]["correct_answer"]) {
+          setScore(score+1);
+        }
+        console.log(score)
         if(questions.length) {
             setIndex(index + 1)
         } else {
@@ -57,10 +85,18 @@ const QuizForm = ({ amountOfQuestions }) => {
       {!question && <button onClick={() => history.push("/profile")}>Back To Some Quizzes</button>}
       {question && (
       <div className="quiz-button-container">
-        <button onClick={handleClick}>{randomAnswer()}</button>
-        <button onClick={handleClick}>{randomAnswer()}</button>
-        <button onClick={handleClick}>{randomAnswer()}</button>
-        <button onClick={handleClick}>{randomAnswer()}</button>
+        <button className="quiz-button__quizform" onClick={() => handleClick(1)}>
+          {Object.values(randomAnswerObj[1])[0]}
+        </button>
+        <button className="quiz-button__quizform" onClick={() => handleClick(2)}>
+          {Object.values(randomAnswerObj[2])[0]}
+        </button>
+        <button className="quiz-button__quizform" onClick={() => handleClick(3)}>
+          {Object.values(randomAnswerObj[3])[0]}
+        </button>
+        <button className="quiz-button__quizform" onClick={() => handleClick(4)}>
+          {Object.values(randomAnswerObj[4])[0]}
+        </button>
       </div>
       )}
     </div>
