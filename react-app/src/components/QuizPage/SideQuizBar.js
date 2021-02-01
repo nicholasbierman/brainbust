6323e783d6933a66a36b16702fc0d5fd346f37f4
Nhu-y Phan <nhucleus@gmail.com>
2021-01-31 22:55:11 -0500

    score bar added

--- react-app/src/components/QuizPage/SideQuizBar.js
+++ react-app/src/components/QuizPage/SideQuizBar.js
@@ -1,14 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-
 
 const SideQuizBar = () => {
   const currentQuiz = useSelector((state) => state.quizzes.currentQuiz);
+  const [average, setAverage] = useState(null);
+
+  useEffect(() => {
+    setAverage((currentQuiz.score / currentQuiz.totalQuestions) * 100);
+  }, [currentQuiz]);
+
+  const [currentScore, finalScore] = useState(null);
+
+  const style = {
+    height: `${average}%`
+  }
+
+
 
   return (
     <div className="score-container">
       <h1>Your Score:</h1>
-      <h1>{currentQuiz.score}</h1>
+        <h2>{currentQuiz.score}</h2>
+      <div className="score-meter-outter">
+        <div className={average <= 40 ? "score-meter low" : average == 100 ? "score-meter full" : "score-meter"} style={style}>
+        </div>
+      </div>
     </div>
   )
 };
